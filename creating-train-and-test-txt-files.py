2884926de1084a3c4c036be896f7f0a7c5ba2d21
wajeehaghazi <wajeehaghazi10@gmail.com>
2025-09-24 18:36:15 +0500

    Update creating-train-and-test-txt-files.py

--- creating-train-and-test-txt-files.py
+++ creating-train-and-test-txt-files.py
@@ -36,7 +36,7 @@ Setting up full path to directory with labelled images
 # r'C:\Users\my_name\Downloads\video-to-annotate'
 # or:
 # 'C:\\Users\\my_name\\Downloads\\video-to-annotate'
-full_path_to_images = '/home/my_name/Downloads/video-to-annotate'
+full_path_to_images = 'Custom_data'
 
 """
 End of:
